58098a56d4d01835cf85d873d4a15e7a1f2c29a9
Afride Hossain <54279218+AfrideHossain@users.noreply.github.com>
2022-01-22 23:18:24 +0600

    new commit

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -33,8 +33,35 @@ const NoteSate = (props) => {
     ];
     const [notes, setNotes] = useState(initNotes);
 
+
+    //Add a note
+    const addNote = (title, description, tag) => {
+        const note = {
+            "_id": "61eade8e8e4588fc06bef9da",
+            "user": "61e2fdf7fd9434bf00fce453",
+            "title": title,
+            "description": description,
+            "tag": tag,
+            "date": "2022-01-21T16:25:50.728Z",
+            "__v": 0
+        };
+        setNotes(notes.concat(note));
+    }
+
+    //Delete a note
+    const deleteNote = (id) => {
+        console.log("Deleting note with id: " + id);
+        const newNotes = notes.filter((note) => { return note._id !== id });
+        setNotes(newNotes);
+    }
+
+    //Edit a note
+    const editNote = () => {
+
+    }
+
     return (
-        <NoteContext.Provider value={{notes, setNotes}}>
+        <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
             {props.children}
         </NoteContext.Provider>
     );
